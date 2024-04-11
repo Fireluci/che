51326c0a5c5c94d55021a47eba0a9af507a35e50
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2024-04-11 19:52:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,13 +22,11 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        buttons = [
-            [
-                InlineKeyboardButton('❓How To Use Me❓', url=f'https://telegram.me/{TUTORIAL}')
-            ]
-               ]
+        buttons = [[
+            InlineKeyboardButton('🔆彡[ @HEROFLiX ]彡🔆', url=f'https://telegram.me/HeroFlix')
+        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
+        await message.reply(script.GRPSTART.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
@@ -40,8 +38,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('❓How To Use Me', url=f'https://telegram.me/{TUTORIAL}'),
-            InlineKeyboardButton('⚜ Updates', url=f'https://telegram.me/{CHNL_LNK}')
+            InlineKeyboardButton('🌟Click Here For Elite🌟', url=f'https://telegram.me/HeroFeedbot')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -81,8 +78,8 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('❓How To Use Me', url=f'https://telegram.me/{TUTORIAL}'),
-            InlineKeyboardButton('⚜ Updates', url=f'https://telegram.me/{CHNL_LNK}')
+            InlineKeyboardButton('🌟Get Elite', url=f'https://telegram.me/HeroFeedbot'),
+            InlineKeyboardButton('⚜ Updates', url='https://telegram.me/HeroFlix')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
