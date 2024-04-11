8bf6990ef0de504465f9acfcbe2e9f3d8fefe422
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2024-04-11 19:55:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,11 +14,11 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '1736204'))
 API_HASH = environ.get('API_HASH', '890d40e0f91a4de32dec2965444b2cbe')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6159823074:AAHOtOeW-CLPtM70xapO0mjlIB7NzH_04yA')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6062946689:AAHxzqCz1dO18kDe9N-y2InrYYvi41bTn28')
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Heroflix:Heroflix@heroflix.6sgi15o.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Ariana")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Ariana')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cheryl:cheryl@cheryl.mpnqm9s.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cheryl")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'cheryl')
 
 DWLD = environ.get("DWLD", 'HeroFlixx/8')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
